392f217fee3d25e218cb2f4255f694b66877f619
ISDP <isdp@localhost.localdomain>
2019-12-24 12:39:11 +0100

    (multi-threaded) Add script showing unused variable Start.threads

---
+++ wielo-nieuzywana-zmienna-threads-pokazanie.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/expect
+set timeout -1
+spawn mkdir DiningPhilosophers_debug/target
+spawn javac DiningPhilosophers_debug/src/main/java/pl/lodz/p/it/isdp/model/PhilosopherModel.java DiningPhilosophers_debug/src/main/java/pl/lodz/p/it/isdp/model/TableModel.java DiningPhilosophers_debug/src/main/java/pl/lodz/p/it/isdp/PhilosopherRunnable.java DiningPhilosophers_debug/src/main/java/pl/lodz/p/it/isdp/Start.java -d target
+spawn jdb -classpath DiningPhilosophers_debug/target -sourcepath DiningPhilosophers_debug/src/main/java pl.lodz.p.it.isdp.Start 9
+expect ">"
+send "watch pl.lodz.p.it.isdp.Start.THREADS_STATE_DELAY\r"
+expect ">"
+send "run\r"
+expect eof
