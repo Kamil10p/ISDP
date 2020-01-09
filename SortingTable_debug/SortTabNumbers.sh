944992fb5fad684059faaf7150816a940d3e091c
Kamil <kamil12p@gmail.com>
2020-01-09 21:15:28 +0100

    fix path

--- SortingTable_debug/SortTabNumbers.sh
+++ SortingTable_debug/SortTabNumbers.sh
@@ -2,7 +2,7 @@
 set timeout 1
 spawn mvn compile
 expect "\\\[INFO\\\] BUILD SUCCESS"
-spawn jdb -classpath ./target -sourcepath ./src/main/java  pl.lodz.p.it.isdp.Start 10
+spawn jdb -classpath ./target/classes -sourcepath ./src/main/java  pl.lodz.p.it.isdp.Start 10
 expect ">"
 send "stop in pl.lodz.p.it.isdp.SortTabNumbers.<init>\r"
 expect ">"
