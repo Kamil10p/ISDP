f542bbd6ba5b6c2e12965acb6799f918bbe93899
Adrian Warcholinski <adrianwarcholinski9@gmail.com>
2020-01-09 21:00:13 +0100

    Fix some scripts

--- DiningPhilosophers_debug/zly-strumien-nfe.sh
+++ DiningPhilosophers_debug/zly-strumien-nfe.sh
@@ -2,9 +2,11 @@
 set timeout -1
 spawn mvn compile
 expect "\\\[INFO\\\] BUILD SUCCESS"
-spawn jdb -classpath DiningPhilosophers_debug/target/classes -sourcepath DiningPhilosophers_debug/src/main/java pl.lodz.p.it.isdp.Start abc
+spawn jdb -classpath target/classes -sourcepath src/main/java pl.lodz.p.it.isdp.Start abc
 expect ">"
 send "catch java.lang.NumberFormatException\r"
 expect ">"
-send "step up"
+send "run\r"
+expect "Exception occurred"
+send "step up\r"
 expect eof
